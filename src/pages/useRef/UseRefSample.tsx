2e9a653dc17ff6901d1ad9347375141a5e695b11
neominalsolutions <neominal.solutions@gmail.com>
2023-11-01 12:38:42 +0300

    UseRef

--- src/pages/useRef/UseRefSample.tsx
+++ src/pages/useRef/UseRefSample.tsx
@@ -3,18 +3,33 @@ import React, { useEffect, useRef, useState } from 'react'
 function UseRefSample() {
   const [inptValue,setInptValue] = useState<string>('');
   // componentin kaç kez render alındığını yakaladığımız değişken.
-  const renderCount = useRef(0);
+  // useRef sayesinde function içerisinde render alındığında sıfırlanan bir değişken değerinin son halini memoisation yapmış olduk. Bu durumda renderCount sıfırlanmadı. her inptValue değişiminde bir önceki değeri koruduğu için doğru bir değer ile çalışmı olduk
+  const renderCount = useRef<number>(0);
+  // let renderCount = 0;
+  console.log('renderCount');
   const onInputChange = (event:any) => {
     setInptValue(event.target.value);
   }
+
+  const onBlurChange = (event:any) => {
+     setInptValue(event.target.value);
+  }
+  // 1. kural Input ile çalışırken OnChange gereksiz render yapıyor.
+
   useEffect(() => {
     renderCount.current  = renderCount.current + 1;
+    // renderCount  = renderCount + 1;
   }, [inptValue])
 
   return (
     <>
-     Input Value: {inptValue}
-     <input onChange={onInputChange} value={inptValue} />
+     Input Value: {inptValue} {' '}
+     Render Count: {renderCount.current}
+     {/* Render Count: {renderCount} */}
+     <hr></hr>
+     <input onChange={onInputChange} defaultValue={inptValue}  />
+     <h6>On Blur</h6>
+     <input onBlur={onBlurChange} defaultValue={inptValue}  />
      <br></br>
      <button onClick={() => {setInptValue('')}}>Clear Input With State</button>
     </>
